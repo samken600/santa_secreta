f34bafdfb17350f70e8443549465ec1862425d7f
redscorp98 <37073183+redscorp98@users.noreply.github.com>
2019-04-23 23:37:56 +0100

    CSS added and some code reformatted

--- front_end_up/secret_santa/src/App.vue
+++ front_end_up/secret_santa/src/App.vue
@@ -1,65 +1,61 @@
 <template>
-	<div id="app">
-		<h1> {{title}} {{value}} </h1>
-		<form id = "login">
-			<label for="email">Email</label>
-			<input type="text" id="email" name="user-email" >
-			<label for="username">Username</label>
-			<input type="text" id="username" name="user-username" >
-			<input type="submit" id="submit-button" 
-			value="Submit" @click="submit">
-		</form>
-		<p>
-			{{input_values}}
-		</p>
-	</div>
+  <div id="app">
+    <h1 class="greeting">{{greeting}}</h1>
+    <form id="login">
+      <label for="email">Email</label>
+      <input type="text" id="email" name="user-email">
+      <label for="username">Username</label>
+      <input type="text" id="username" name="user-username">
+      <input type="submit" id="submit-button" value="Submit" @click="submit">
+    </form>
+    <p>{{input_values}}</p>
+  </div>
 </template>
 
 <script>
-import firebase from 'firebase';
+import firebase from "firebase";
 
 let config = {
-	apiKey: "AIzaSyAUT063UC7cTHbd4fBnu3lDs7CYrRWawhA",
-	authDomain: "santa-secreta.firebaseapp.com",
-	databaseURL: "https://santa-secreta.firebaseio.com/",
-	projectId: "santa-secreta",
-	storageBucket: "santa-secreta.appspot.com",
-	messagingSenderId: "602714974736"
+  apiKey: "AIzaSyAUT063UC7cTHbd4fBnu3lDs7CYrRWawhA",
+  authDomain: "santa-secreta.firebaseapp.com",
+  databaseURL: "https://santa-secreta.firebaseio.com/",
+  projectId: "santa-secreta",
+  storageBucket: "santa-secreta.appspot.com",
+  messagingSenderId: "602714974736"
 };
 
 firebase.initializeApp(config);
-let functions = firebase.app().functions('europe-west1');
+let functions = firebase.app().functions("europe-west1");
 
 export default {
-	name: 'app',
-	data () {
-		return {
-			title: "App section!",
-			value: "1",
-			input_values: "",
-		}
-	},
-	methods:{
-		inc: function(){
-			this.value++;
-		},
-		take: function(hold){
-			this.input_values = hold;
-		},
-		submit: function(){
-			let uemail = document.getElementById('email').value;
-			let uusername = document.getElementById('username').value;
+  name: "app",
+  data() {
+    return {
+      greeting: "Log in to Secret Santa"
+    };
+  },
+  methods: {
+    inc: function() {
+      this.value++;
+    },
+    take: function(hold) {
+      this.input_values = hold;
+    },
+    submit: function() {
+      let uemail = document.getElementById("email").value;
+      let uusername = document.getElementById("username").value;
 
-			let login = functions.httpsCallable('login');
-			login({email: uemail, username: uusername})
-			.then(function(result){
-				console.log(result);
-			}).catch(function(error){
-				console.error(error);
-			});
-		},
-	}
-}
+      let login = functions.httpsCallable("login");
+      login({ email: uemail, username: uusername })
+        .then(function(result) {
+          console.log(result);
+        })
+        .catch(function(error) {
+          console.error(error);
+        });
+    }
+  }
+};
 </script>
 
 <style>
