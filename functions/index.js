13f0f220dc1b7a349031975f6223601c91999d6e
samken600 <samuelkendall@hotmail.co.uk>
2019-04-23 19:40:40 +0100

    Login func completed, untested

--- functions/index.js
+++ functions/index.js
@@ -11,3 +11,57 @@ let db = firebase.firestore();
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.login = functions.region('europe-west1').https.onCall(async (data, context) => {
+    const email = data.email.trim();
+    const username = data.username.trim();
+    const users = db.collection('users');
+
+    try{
+        const querySnapshot = await  users.where("email", "==", email)
+            .where("username", "==", username)
+            .get();
+        let userId = null;
+        if(querySnapshot.size > 0) {
+            querySnapshot.forEach(function (doc) {
+                userId = doc.id;
+            });
+        } else {
+            const emailQuerySnapshot = await  users.where("email", "==", email).get();
+            const usernameQuerySnapshot = await  users.where("username", "==", username).get();
+            if(emailQuerySnapshot.size === 0 && usernameQuerySnapshot.size === 0) {
+                const doc = await users.add({
+                    email: email,
+                    username: username
+                });
+
+                userId = doc.id;
+            } else {
+                console.warn("Email/username exists");
+            }
+        }
+        return userId;
+
+
+    } catch (e) {
+        console.error(e);
+        return null;
+    }
+});
+
+/*
+exports.create_list = functions.region('europe-west1').https.onCall(async (data, context) => {
+
+
+});
+
+exports.delete_list = functions.region('europe-west1').https.onCall(async (data, context) => {
+
+
+});
+
+exports.reshuffle_list = functions.region('europe-west1').https.onCall(async (data, context) => {
+
+
+});
+*/
