7e94ca54e26e7ea704fb9236971615a98d28041d
redscorp98 <37073183+redscorp98@users.noreply.github.com>
2019-04-24 01:49:16 +0100

    Changed the files to add vue-router and changes urls and stuff

--- front_end_up/secret_santa/src/List.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>HI</h1>
-</template>
--- front_end_up/secret_santa/src/main.js
+++ front_end_up/secret_santa/src/main.js
@@ -1,20 +1,26 @@
 import Vue from "vue";
 import App from "./App.vue";
-import List from "./List.vue";
-import firebase from "firebase";
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
 
-var th = true;
+const Main = { template: "<div><h1>CRAZY Main</h1></div>" };
+const Log = { template: "<div><h1>CRAZY Log</h1></div>" };
 
-var main = new Vue({
-  el: "#app",
-  render: h => h(App)
-  // renders the app.vue and puts in index
+const routes = [
+  { path: "/home", component: App },
+  { path: "/log", component: Log }
+];
+
+const router = new VueRouter({
+  routes
 });
-var one = new Vue({
-  el: "#list",
-  render: h => h(List),
-  data: {
-    title: "sfff"
-  }
-  // renders the app.vue and puts in index
-});
+
+const app = new Vue({
+  router
+}).$mount("#app");
+
+// var main = new Vue({
+//   el: "#app",
+//   render: h => h(App)
+//   // renders the app.vue and puts in index
+// });
