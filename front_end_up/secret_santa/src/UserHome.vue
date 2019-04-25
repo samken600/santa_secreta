6fb61ade7f867c9914419dbceb87137ac7e3dca2
redscorp98 <37073183+redscorp98@users.noreply.github.com>
2019-04-25 17:49:24 +0100

    home view shows the list in the style expected from db and the + button changes view

--- front_end_up/secret_santa/src/UserHome.vue
+++ front_end_up/secret_santa/src/UserHome.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
     <h1>{{username}}'s Home</h1>
-    {{userID}}
-
+    <p>{{userID}}</p>
+    <div id = "list" v-if="ShowList == false" v-for="list in RealList">
+      <h3 class="ListTitle">{{list.name}}</h3>
+      <p class ="ListPerson" v-for="person in list.persons">{{person}}</p>
+    </div>
+    <div v-if="ShowList == true">
+      <h3>
+        {{ PresentList }}
+      </h3>
+    </div>
+    <button id=add v-on:click="add">
+      +
+    </button>
   </div>
 </template>
 
@@ -10,9 +21,27 @@
 
 export default {
   name: "userhome",
-  props: ['userID', 'username']
-}
-
+  data() {
+    return {
+      RealList: [
+        {name: 'Fun List', persons: ['sam', 'mike', 'ala']},
+        {name: 'Bad list', persons: ['sam1', 'sam2']},
+        {name: 'awesome list', persons: ['jacob', "jacob's friend", "jacob's other friend", "steve", "pikachu"]},
+      ],
+      ShowList: false,
+      PresentList: 'THIS LIST',
+    };
+  },
+  methods:{
+    add: function(){
+      console.log(this.userID);
+      this.ShowList = !this.ShowList;
+      //use this function to to add to list
+    }
+  },
+  props: ["userID", "username"]
+};
 </script>
 
 <!-- This is how you comment html -->
+
